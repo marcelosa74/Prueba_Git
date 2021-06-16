05ac0f10b5200db4f5b1a31c609e73b3315842cf
Marcelo Salazar <marcelosalazara@hotmail.com>
2021-06-16 18:32:01 -0500

    Carga de Archivos de Omision

--- hanoi.java
+++ hanoi.java
@@ -1,3 +1,4 @@
+//Comentario prueba 1
 /* Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
 Es posible que el codigo generado no sea completamente correcto. Si encuentra
 errores por favor reportelos en el foro (http://pseint.sourceforge.net). */
