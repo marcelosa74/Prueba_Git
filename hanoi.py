fbbc9569a6ec8a2862a7cf50e2840befb69b7065
Marcelo Salazar <marcelosalazara@hotmail.com>
2021-06-16 17:41:17 -0500

    Agregando cambios archivo hanoi.py

--- hanoi.py
+++ hanoi.py
@@ -2,6 +2,7 @@
 # 16-06-2021
 # Nuevo comentario
 # Nuevo comentario2
+# Nuevo comentario3
 # Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
 # Es posible que el codigo generado no sea completamente correcto. Si encuentra
 # errores por favor reportelos en el foro (http://pseint.sourceforge.net).
