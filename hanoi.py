81a7c9fe5909dfc26416f1a03ee174b1316b1cb2
marcelosa74 <62671513+marcelosa74@users.noreply.github.com>
2021-06-16 16:47:34 -0500

    Update hanoi.py

--- hanoi.py
+++ hanoi.py
@@ -1,3 +1,5 @@
+# Comentario para prueba de GITHUB
+# 16-06-2021
 # Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
 # Es posible que el codigo generado no sea completamente correcto. Si encuentra
 # errores por favor reportelos en el foro (http://pseint.sourceforge.net).
