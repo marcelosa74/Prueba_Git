0e322af985b686bd6fa575b72f05b1bd44b0b945
Marcelo Salazar <marcelosalazara@hotmail.com>
2021-06-16 17:26:31 -0500

    Nuevos cambios

--- hanoi.py
+++ hanoi.py
@@ -1,5 +1,6 @@
 # Comentario para prueba de GITHUB
 # 16-06-2021
+# Nuevo comentario
 # Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
 # Es posible que el codigo generado no sea completamente correcto. Si encuentra
 # errores por favor reportelos en el foro (http://pseint.sourceforge.net).
